4a35653cbb5124ac17cbe269d716c44dfc1353a6
Miguel Lopez <miguellopez32@gmail.com>
2023-01-27 08:41:09 -0300

    Cuando se hace click se cierra automaticamente el menu hamburguesa

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,33 @@
 const menuHamb = document.querySelector('#menuHamb')
 const abrir = document.querySelector('#abrir')
 const cerrar = document.querySelector('#cerrar')
+const enlace1 = document.querySelector('#enlace1')
+const enlace2 = document.querySelector('#enlace2')
+const enlace3 = document.querySelector('#enlace3')
+const enlace4 = document.querySelector('#enlace4')
+const enlace5 = document.querySelector('#enlace5')
+const enlace6 = document.querySelector('#enlace6')
 abrir.addEventListener('click',()=> {
     menuHamb.classList.add('menuVisible')
 })
 cerrar.addEventListener('click',()=> {
+    menuHamb.classList.remove('menuVisible')
+})
+enlace1.addEventListener('click',()=> {
+    menuHamb.classList.remove('menuVisible')
+})
+enlace2.addEventListener('click',()=> {
+    menuHamb.classList.remove('menuVisible')
+})
+enlace3.addEventListener('click',()=> {
+    menuHamb.classList.remove('menuVisible')
+})
+enlace4.addEventListener('click',()=> {
+    menuHamb.classList.remove('menuVisible')
+})
+enlace5.addEventListener('click',()=> {
+    menuHamb.classList.remove('menuVisible')
+})
+enlace6.addEventListener('click',()=> {
     menuHamb.classList.remove('menuVisible')
 })
