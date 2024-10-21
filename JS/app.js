3a59763688ad8490388be8fe91908a7f9bb0d8a3
Miguel Lopez <miguellopez32@gmail.com>
2024-10-20 22:24:12 -0300

    Segunda prueba envio de formulario

--- JS/app.js
+++ JS/app.js
@@ -1,48 +1,54 @@
-const menuHamb = document.querySelector('#menuHamb')
-const abrir = document.querySelector('#abrir')
-const cerrar = document.querySelector('#cerrar')
-const enlace1 = document.querySelector('#enlace1')
-const enlace2 = document.querySelector('#enlace2')
-const enlace3 = document.querySelector('#enlace3')
-const enlace4 = document.querySelector('#enlace4')
-const enlace5 = document.querySelector('#enlace5')
-const enlace6 = document.querySelector('#enlace6')
-abrir.addEventListener('click',()=> {
-    menuHamb.classList.add('menuVisible')
-})
-cerrar.addEventListener('click',()=> {
-    menuHamb.classList.remove('menuVisible')
-})
-enlace1.addEventListener('click',()=> {
-    menuHamb.classList.remove('menuVisible')
-})
-enlace2.addEventListener('click',()=> {
-    menuHamb.classList.remove('menuVisible')
-})
-enlace3.addEventListener('click',()=> {
-    menuHamb.classList.remove('menuVisible')
-})
-enlace4.addEventListener('click',()=> {
-    menuHamb.classList.remove('menuVisible')
-})
-enlace5.addEventListener('click',()=> {
-    menuHamb.classList.remove('menuVisible')
-})
-enlace6.addEventListener('click',()=> {
-    menuHamb.classList.remove('menuVisible')
-})
+const menuHamb = document.querySelector('#menuHamb');
+const abrir = document.querySelector('#abrir');
+const cerrar = document.querySelector('#cerrar');
+const enlace1 = document.querySelector('#enlace1');
+const enlace2 = document.querySelector('#enlace2');
+const enlace3 = document.querySelector('#enlace3');
+const enlace4 = document.querySelector('#enlace4');
+const enlace5 = document.querySelector('#enlace5');
+const enlace6 = document.querySelector('#enlace6');
+
+abrir.addEventListener('click', () => {
+    menuHamb.classList.add('menuVisible');
+});
+
+cerrar.addEventListener('click', () => {
+    menuHamb.classList.remove('menuVisible');
+});
+
+enlace1.addEventListener('click', () => {
+    menuHamb.classList.remove('menuVisible');
+});
+
+enlace2.addEventListener('click', () => {
+    menuHamb.classList.remove('menuVisible');
+});
+
+enlace3.addEventListener('click', () => {
+    menuHamb.classList.remove('menuVisible');
+});
+
+enlace4.addEventListener('click', () => {
+    menuHamb.classList.remove('menuVisible');
+});
+
+enlace5.addEventListener('click', () => {
+    menuHamb.classList.remove('menuVisible');
+});
+
+enlace6.addEventListener('click', () => {
+    menuHamb.classList.remove('menuVisible');
+});
+
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Evita que el formulario se envíe por defecto
-    
+
     // Obtiene los valores de los campos del formulario
     const nombre = document.getElementById('nombre').value;
     const email = document.getElementById('email').value;
     const mensaje = document.getElementById('mensaje').value;
 
-    
-    
-    
-    fetch('url_de_tu_servidor', {
+    fetch('https://formspree.io/f/mbjepzer', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -66,4 +72,3 @@ document.getElementById('contactForm').addEventListener('submit', function(event
         alert('Hubo un error al enviar el mensaje. Por favor, inténtalo nuevamente.');
     });
 });
-
